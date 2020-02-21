0aec95010d76dc2e3f2e144950ae4c9b47744b77
Itay Almog <itay2828@gmail.com>
2020-02-21 11:59:29 +0200

    some cleanup in the loader

--- src/loaders/loader.c
+++ src/loaders/loader.c
@@ -6,21 +6,17 @@
 
 #include "loader.h"
 
+void load_linux_binary(boot_entry_t* entry);
+void load_tboot_binary(boot_entry_t* entry);
+void load_uefi_binary(boot_entry_t* entry);
+
+static void(*loaders[])(boot_entry_t* entry) = {
+    [BOOT_LINUX] = load_linux_binary,
+    [BOOT_TBOOT] = load_tboot_binary,
+    [BOOT_UEFI] = load_uefi_binary,
+};
+
 void load_binary(boot_entry_t* entry) {
-    switch(entry->protocol) {
-        case BOOT_LINUX:
-            load_linux_binary(entry);
-            break;
-
-        case BOOT_TBOOT:
-            load_tboot_binary(entry);
-            break;
-
-        case BOOT_UEFI:
-            load_uefi_binary(entry);
-            break;
-
-        default:
-            ASSERT(FALSE);
-    }
+    ASSERT(ARRAY_SIZE(loaders) > entry->protocol);
+    loaders[entry->protocol](entry);
 }
--- src/loaders/uefi_loader.h
+++
@@ -1,8 +0,0 @@
-#ifndef TOMATBOOT_UEFI_UEFI_LOADER_H
-#define TOMATBOOT_UEFI_UEFI_LOADER_H
-
-#include <config.h>
-
-void load_uefi_binary(boot_entry_t* entry);
-
-#endif //TOMATBOOT_UEFI_UEFI_LOADER_H
